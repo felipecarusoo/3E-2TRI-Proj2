9c10eccfd465706cfd1ca2c5102289e258996e80
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-08-06 12:25:10 +0000

    .

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
 const perguntas = [
     {
         enunciado: "",
@@ -16,3 +22,34 @@ const perguntas = [
         enunciado: "repete o padrao superior para todas perguntas",
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta () {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[length];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativa();
+}
+
+function respostaSelecionada (opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+
+
+function mostraResultado () {
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
